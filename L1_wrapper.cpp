3401a927af645b58a339bfbd25e6d7304691bfd6
Axel Curmi <axel.curmi@gmail.com>
2021-03-11 23:18:12 +0100

    feat: add encrypt and decrypt

--- L1_wrapper.cpp
+++ L1_wrapper.cpp
@@ -3,30 +3,30 @@
 
 #include <stdio.h>
 
-struct L1_handler
+struct L1_handle
 {
 	void *obj;
 };
 
-L1_handler_t *L1_create()
+L1_handle_t *L1_Create()
 {
-	L1_handler_t *l1;
+	L1_handle_t *l1;
 	L1 *obj;
 
-	l1 = (L1_handler_t *)malloc(sizeof(L1_handler));
+	l1 = (L1_handle_t *)malloc(sizeof(L1_handle));
 	obj = new L1();
 
 	l1->obj = obj;
 	return l1;
 }
 
-void L1_destroy(L1_handler_t *l1)
+void L1_Destroy(L1_handle_t *l1)
 {
 	delete (L1 *)l1->obj;
 	free(l1);
 }
 
-int8_t L1_Login(L1_handler_t *l1, const uint8_t *pin, uint16_t access,
+int8_t L1_Login(L1_handle_t *l1, const uint8_t *pin, uint16_t access,
 				bool force)
 {
 	L1 *obj = (L1 *)l1->obj;
@@ -41,7 +41,7 @@ int8_t L1_Login(L1_handler_t *l1, const uint8_t *pin, uint16_t access,
 	return 0;
 }
 
-int8_t L1_Logout(L1_handler_t *l1)
+int8_t L1_Logout(L1_handle_t *l1)
 {
 	L1 *obj = (L1 *)l1->obj;
 	try
@@ -55,7 +55,7 @@ int8_t L1_Logout(L1_handler_t *l1)
 	return 0;
 }
 
-int8_t L1_CryptoSetTimeNow(L1_handler_t *l1)
+int8_t L1_CryptoSetTimeNow(L1_handle_t *l1)
 {
 	L1 *obj = (L1 *)l1->obj;
 	try
@@ -69,7 +69,41 @@ int8_t L1_CryptoSetTimeNow(L1_handler_t *l1)
 	return 0;
 }
 
-int8_t L1_CryptoInit(L1_handler_t *l1, uint16_t algorithm, uint16_t mode,
+int8_t L1_Encrypt(L1_handle_t *l1, size_t dataInLen, uint8_t* dataIn,
+				  size_t* dataOutLen, uint8_t* dataOut, uint16_t algorithm,
+				  uint16_t mode, uint32_t keyID)
+{
+	L1 *obj = (L1 *)l1->obj;
+	try
+	{
+		obj->L1Encrypt(dataInLen, dataIn, dataOutLen, dataOut, algorithm,
+					   mode, keyID);
+	}
+	catch (...)
+	{
+		return -1;
+	}
+	return 0;
+}
+
+int8_t L1_Decrypt(L1_handle_t *l1, size_t dataInLen, uint8_t* dataIn,
+				 size_t* dataOutLen, uint8_t* dataOut, uint16_t algorithm,
+				 uint16_t mode, uint32_t keyID)
+{
+	L1 *obj = (L1 *)l1->obj;
+	try
+	{
+		obj->L1Decrypt(dataInLen, dataIn, dataOutLen, dataOut, algorithm,
+					   mode, keyID);
+	}
+	catch (...)
+	{
+		return -1;
+	}
+	return 0;
+}
+
+int8_t L1_CryptoInit(L1_handle_t *l1, uint16_t algorithm, uint16_t mode,
 					 uint32_t keyID, uint32_t* sessionID)
 {
 	L1 *obj = (L1 *)l1->obj;
@@ -86,7 +120,7 @@ int8_t L1_CryptoInit(L1_handler_t *l1, uint16_t algorithm, uint16_t mode,
 	return 0;
 }
 
-int8_t L1_CryptoUpdate(L1_handler_t *l1, uint32_t sessionID, uint16_t flags,
+int8_t L1_CryptoUpdate(L1_handle_t *l1, uint32_t sessionID, uint16_t flags,
                            uint16_t dataInLen, uint8_t* dataIn,
                            uint16_t* dataOutLen, uint8_t* dataOut)
 {
--- L1_wrapper.h
+++ L1_wrapper.h
@@ -5,23 +5,29 @@
 
 extern "C"
 {
-    struct L1_handler;
-    typedef struct L1_handler L1_handler_t;
+    struct L1_handle;
+    typedef struct L1_handle L1_handle_t;
 
-    L1_handler_t *L1_create();
-    void L1_destroy(L1_handler_t *l1);
+    L1_handle_t *L1_Create();
+    void L1_Destroy(L1_handle_t *l1);
 
-    int8_t L1_Login(L1_handler_t *l1, const uint8_t *pin, uint16_t access,
+    int8_t L1_Login(L1_handle_t *l1, const uint8_t *pin, uint16_t access,
                     bool force);
-    int8_t L1_Logout(L1_handler_t *l1);
+    int8_t L1_Logout(L1_handle_t *l1);
 
-    int8_t L1_CryptoSetTimeNow(L1_handler_t *l1);
-
-    int8_t L1_CryptoInit(L1_handler_t *l1, uint16_t algorithm, uint16_t mode,
+    int8_t L1_CryptoSetTimeNow(L1_handle_t *l1);
+    int8_t L1_CryptoInit(L1_handle_t *l1, uint16_t algorithm, uint16_t mode,
                          uint32_t keyID, uint32_t* sessionID);
-    int8_t L1_CryptoUpdate(L1_handler_t *l1, uint32_t sessionID, uint16_t flags,
+    int8_t L1_CryptoUpdate(L1_handle_t *l1, uint32_t sessionID, uint16_t flags,
                            uint16_t dataInLen, uint8_t* dataIn,
                            uint16_t* dataOutLen, uint8_t* dataOut);
+
+    int8_t L1_Encrypt(L1_handle_t *l1, size_t dataInLen, uint8_t* dataIn,
+                      size_t* dataOutLen, uint8_t* dataOut, uint16_t algorithm,
+                      uint16_t mode, uint32_t keyID);
+    int8_t L1_Decrypt(L1_handle_t *l1, size_t dataInLen, uint8_t* dataIn,
+                      size_t* dataOutLen, uint8_t* dataOut, uint16_t algorithm,
+                      uint16_t mode, uint32_t keyID);
 }
 
 #endif // !_L1_WRAPPER_H

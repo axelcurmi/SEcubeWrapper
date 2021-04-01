5e3564e68438d02b22860528d574e545e3b4f519
Axel Curmi <axel.curmi@gmail.com>
2021-04-01 20:56:08 +0200

    feat: add initialise

--- build.bat
+++ build.bat
@@ -16,6 +16,8 @@ g++ -O3 -g3 -fPIC -Wall -c -o obj\L1.o SEcubeSources\L1\L1.cpp
 g++ -O0 -g3 -fPIC -Wall -c -o obj\L0_wrapper.o L0_wrapper.cpp
 g++ -O0 -g3 -fPIC -Wall -c -o obj\L1_wrapper.o L1_wrapper.cpp
 
+g++ -O0 -c -o obj\initialise.o initialise.cpp
+
 g++ -shared -fPIC -o lib/SEcubeWrapper.dll ^
     obj\L0.o ^
     obj\L0_commodities.o ^
@@ -31,3 +33,18 @@ g++ -shared -fPIC -o lib/SEcubeWrapper.dll ^
     obj\L1.o ^
     obj\L0_wrapper.o ^
     obj\L1_wrapper.o
+
+g++ -o bin\initialise.exe ^
+    obj\L0.o ^
+    obj\L0_commodities.o ^
+    obj\L0_communication.o ^
+    obj\L0_provision.o ^
+    obj\L0_base.o ^
+    obj\sha256.o ^
+    obj\aes256.o ^
+    obj\pbkdf2.o ^
+    obj\L1_base.o ^
+    obj\L1_login_logout.o ^
+    obj\L1_security.o ^
+    obj\L1.o ^
+    obj\initialise.o
